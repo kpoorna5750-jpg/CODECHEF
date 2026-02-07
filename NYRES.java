ea828bc3d5beff63d97950e13297dde0e5ac1f44
kpoorna5750-jpg <kpoorna5750@gmail.com>
2026-02-07 16:03:32 +0530

    Add files via upload

---
+++ NYRES.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the number of pushups per day:");
+	    int a=sc.nextInt();
+	    System.out.println(31*a);
+	    
+
+	}
+}
