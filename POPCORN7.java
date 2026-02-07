0cdd1341df9e840ffa62b9526354d9942f10e291
kpoorna5750-jpg <kpoorna5750@gmail.com>
2026-02-07 16:23:29 +0530

    Add files via upload

---
+++ POPCORN7.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the no.of ruppes chef has:");
+		int a=sc.nextInt();
+		if(a>=100){
+		    System.out.println((a-100)/50);
+		    
+		    
+		}
+
+	}
+}
