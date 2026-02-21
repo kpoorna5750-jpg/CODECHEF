4e411d0d7f4dd8e1bbc7797af1ed5e908615f806
kpoorna5750-jpg <kpoorna5750@gmail.com>
2026-02-21 15:06:54 +0530

    Add files via upload

---
+++ RECSQ.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int c=sc.nextInt();
+		int rec=a*b;
+		int sq=c*c;
+		if(rec==sq){
+		    System.out.println("Yes");
+		    
+		}
+		else{
+		    System.out.println("No");
+		}
+
+	}
+}
