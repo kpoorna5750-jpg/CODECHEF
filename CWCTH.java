2aa8885e85a2863bbddd52504c40dbec09203ce3
kpoorna5750-jpg <kpoorna5750@gmail.com>
2026-02-22 15:43:52 +0530

    Add files via upload

---
+++ CWCTH.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		if(b>=3*a){
+		    System.out.println("Rain");
+		}
+		else{
+		    System.out.println("Dry");
+		}
+
+	}
+}
