1c8d223d12da2b867cb03e1229bc0fe915f677c5
kpoorna5750-jpg <kpoorna5750@gmail.com>
2026-02-22 16:07:38 +0530

    Add files via upload

---
+++ SKONG.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int x=sc.nextInt();
+		int h=sc.nextInt();
+		int play=((x-4)*24)+h;
+		System.out.println(play);
+
+	}
+}
