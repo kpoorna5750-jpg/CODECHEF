1e12ffa12760b0574e4ceb31897b9d9cd9661a50
kpoorna5750-jpg <kpoorna5750@gmail.com>
2026-02-23 19:02:53 +0530

    Add files via upload

---
+++ DIWALIDISC.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		if(b>a){
+		    System.out.println("0");
+		}
+        else{
+            System.out.println(a-b);
+        }
+	}
+}
---
+++ P1209.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int r=sc.nextInt();
+		if(r<=4){
+		    System.out.println("Yes");
+		}
+		else{
+		    System.out.println("no");
+		}
+
+	}
+}
