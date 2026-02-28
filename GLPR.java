439413c49be4e8084d7830a938da424a546798cd
kpoorna5750-jpg <kpoorna5750@gmail.com>
2026-02-28 16:16:42 +0530

    Add files via upload

---
+++ GLPR.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner sc=new Scanner(System.in);
+	    int x=sc.nextInt();
+	    int y=sc.nextInt();
+	    if(y<=2*x){
+	        System.out.println("metal");
+	    }
+	    else{
+	        System.out.println("plastic");
+	    }
+
+	}
+}
---
+++ LUCKYSEVEN.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		String str=sc.next();
+		System.out.println(str.charAt(6));
+
+	}
+}
