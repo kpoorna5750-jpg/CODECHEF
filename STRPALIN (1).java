79f32d3cedc3d502d4790607dd0920393bd16a19
kpoorna5750-jpg <kpoorna5750@gmail.com>
2026-03-07 11:53:22 +0530

    Add files via upload

---
+++ STRPALIN (1).java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner sc=new Scanner(System.in);
+	    int t=sc.nextInt();
+	    fot(int i=0;i<=t;i++){
+	        String a=sc.next();
+	        String b=sc.next();
+	    }
+	    String s1=a.substring();
+	    String s2=b.subString();
+	    String rev=" ";
+	    for(int i=0;i<=s.length()-1;i--){
+	        rev+=s.charAt(i);
+	    }
+        if(s1+s2==rev){
+            System.out.println("Yes");
+        }
+        else{
+            System.out.println("No");
+        }
+	}
+}
