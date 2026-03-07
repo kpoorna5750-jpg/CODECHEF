91d24c0b0c9969f739b00f1405fe4f835754b8b7
kpoorna5750-jpg <kpoorna5750@gmail.com>
2026-03-07 15:08:40 +0530

    Add files via upload

---
+++ CREDSCORE.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int x=sc.nextInt();
+		if(x>=750){
+		    System.out.println("yES");
+		}
+		else{
+		    System.out.println("NO");
+		}
+		    
+	
+		
+
+	}
+}
---
+++ LEGSP.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int m=sc.nextInt();
+		if(n<m){
+		    System.out.println("Yes");
+		}
+		else{
+		    System.out.println("NO");
+		}
+
+	}
+}
